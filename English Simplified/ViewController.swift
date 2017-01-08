feee4e3a75bdc7cc4de08c7fbcaed0cf8b5c019c
Funde, Rohit | Rohit | PAYSD <rohit.funde@rakuten.com>
2017-01-08 23:08:36 +0900

    Initial Commit

---
+++ English Simplified/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  English Simplified
+//
+//  Created by Funde, Rohit on 1/8/17.
+//  Copyright © 2017 Funde, Rohit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
