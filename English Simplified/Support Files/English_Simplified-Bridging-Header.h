78f18878e73568f40822213db766dd30cf415c00
rohit.funde <rrfunde@gmail.com>
2017-03-10 14:49:43 +0900

    added bridging header

---
+++ English Simplified/Support Files/English_Simplified-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  EnglishSimplified-Bridging-Header.h
+//  English Simplified
+//
+//  Created by Pandya, Advaita on 2/17/17.
+//  Copyright © 2017 Funde, Rohit. All rights reserved.
+//
+
+#ifndef EnglishSimplified_Bridging_Header_h
+#define EnglishSimplified_Bridging_Header_h
+
+#import "YTPlayerView.h"
+
+#endif /* EnglishSimplified_Bridging_Header_h */
